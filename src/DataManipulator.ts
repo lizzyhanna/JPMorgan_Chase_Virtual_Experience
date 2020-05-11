4af35817ba25507ed58ebb239bb23b3ffb131a19
lizzyhanna <lhanna@smu.edu>
2020-05-11 16:05:13 -0500

    first commit

---
+++ src/DataManipulator.ts
@@ -0,0 +1,36 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]): Row{
+
+    //manipulate values from server
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+    const ratio = priceABC / priceDEF; //calculate ratio between ABC and DEF
+    const upperBound = 1 + 0.05; //+5%
+    const lowerBound = 1 - 0.05; //-5%
+
+    return{
+      //return the values to be shown on graph
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio: undefined,
+    };
+  }
+}
